dddc2512e45dbfb139d18a2fd695d22452fe12f5
Vasco Raminhos <vascoraminhos@hotmail.com>
2022-09-26 21:10:58 +0300

    ~Changed CSV delimiter to a semicolon

--- backend/utils/csv-generator.js
+++ backend/utils/csv-generator.js
@@ -1,16 +1,18 @@
+const DELIMITER = ';'
+
 function generateCSV(data, fields) {
 	let csvString = "\uFEFF";
 
 	//Adding the field names
 	for (let i = 0; i < fields.length; i++) {
-		csvString += '"' + fields[i].name + '",';
+		csvString += '"' + fields[i].name + '"' + DELIMITER;
 	}
 	csvString = csvString.slice(0, -1);
 	csvString += "\n";
 
 	for (let i = 0; i < data.length; i++) {
 		for (let j = 0; j < fields.length; j++) {
-			csvString += '"' + data[i][fields[j].name] + '",';
+			csvString += '"' + data[i][fields[j].name] + '"' + DELIMITER;
 		}
 		csvString = csvString.slice(0, -1);
 		csvString += "\n";
